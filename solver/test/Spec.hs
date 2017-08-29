e6f94289470ca0fde07424f0645267fc13879c49
Leandro T. C. Melo <LTCMELO@GMAIL.COM>
2017-08-29 14:38:35 -0300

    Add one more test

--- solver/test/Spec.hs
+++ solver/test/Spec.hs
@@ -257,6 +257,7 @@ tests = testGroup "Unit tests"
                   , doTest "T210.c" -- Cast with void: (void)x.
                   , doTest "T211.c" -- Dont crash on unnamed param.
                   , doTest "T212.c" -- Postpone scalar_t__ unification.
+                  , doTest "T213.c" -- Like 212, but with 0 instead of NULL.
                   ]
 
 doTest s
