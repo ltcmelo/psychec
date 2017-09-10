c8b0cd495626005e093bbece86a950866637bffc
Leandro T. C. Melo <LTCMELO@GMAIL.COM>
2017-09-10 20:41:18 -0300

    Add copyright on recently modified files

--- frontend/NameVisitor.h
+++ frontend/NameVisitor.h
@@ -1,5 +1,7 @@
 // Copyright (c) 2008 Roberto Raggi <roberto.raggi@gmail.com>
-// Modifications: Copyright (c) 2016 Leandro T. C. Melo (ltcmelo@gmail.com)
+//
+// Modifications:
+// Copyright (c) 2016,17 Leandro T. C. Melo (ltcmelo@gmail.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
