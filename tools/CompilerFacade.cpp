fe6248e5ebe7db35a006ba9c7e22b6b13fc52d00
Andrew V. Jones <andrewvaughanj@gmail.com>
2021-03-21 20:55:27 +0000

    Implementing support for preprocessing large files #9
    Signed-off-by: Andrew V. Jones <andrewvaughanj@gmail.com>

--- tools/CompilerFacade.cpp
+++ tools/CompilerFacade.cpp
@@ -23,6 +23,9 @@
 #include "Process.h"
 
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <unistd.h> /* for getpid */
 
 using namespace psy;
 
@@ -38,15 +41,35 @@ CompilerFacade::CompilerFacade(const std::string& hostCC,
 
 std::pair<int, std::string> CompilerFacade::preprocess(const std::string& source)
 {
-    std::string in = "cat << 'EOF' | ";
-    in += hostCC_;
+    // get the current process id
+    auto curr_pid = getpid();
+
+    // Base filename
+    auto base_name("psyche_" + std::to_string(curr_pid));
+
+    // Temporary directory name
+    auto tmp_dir(std::filesystem::temp_directory_path());
+
+    // Temporary filename
+    auto tmp_name(tmp_dir / base_name);
+
+    // Write our source to our temporary file
+    std::ofstream tmp_stream(tmp_name);
+    tmp_stream << source;
+
+    std::string in = hostCC_;
     in += macroSetup();
     in += " ";
     in += "-std=" + std_ + " ";
-    in += "-E -x c -CC -";
-    in += "\n" + source + "\nEOF";
+    in += "-E -x c -CC ";
+    in += tmp_name;
 
-    return Process().execute(in);
+    auto ret = Process().execute(in);
+
+    // remove the temporary file
+    std::remove(tmp_name.c_str());
+
+    return ret;
 }
 
 std::string CompilerFacade::macroSetup() const
