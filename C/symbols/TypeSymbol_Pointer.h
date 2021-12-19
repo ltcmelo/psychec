1875bed632c99305ca00099ead7f6beabf6fc5a6
Leandro T. C. Melo <LTCMELO@GMAIL.COM>
2021-12-19 16:48:57 -0300

    array and pointer types

---
+++ C/symbols/TypeSymbol_Pointer.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2021 Leandro T. C. Melo <ltcmelo@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef PSYCHE_C_SYMBOL_POINTER_TYPE_H__
+#define PSYCHE_C_SYMBOL_POINTER_TYPE_H__
+
+#include "Symbol_Type.h"
+
+namespace psy {
+namespace C {
+
+class PSY_C_API PointerTypeSymbol : public TypeSymbol
+{
+private:
+    friend class Binder;
+
+    PointerTypeSymbol(const SyntaxTree* tree,
+                     const Scope* outerScope,
+                     const Symbol* containingSym);
+};
+
+} // C
+} // psy
+
+#endif
