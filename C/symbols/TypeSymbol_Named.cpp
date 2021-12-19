7cda5f5400bfde656346ff8a980ff87a988dcc1e
Leandro T. C. Melo <LTCMELO@GMAIL.COM>
2021-12-19 16:45:38 -0300

    type symbol file/name reort

--- C/symbols/TypeSymbol_Named.cpp
+++ C/symbols/TypeSymbol_Named.cpp
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#include "Symbol_NamedType.h"
+#include "TypeSymbol_Named.h"
 
 using namespace psy;
 using namespace C;
