d63d7e500af075cc263f3d87772126afb82a36cb
Leandro T. C. Melo <LTCMELO@GMAIL.COM>
2022-02-18 16:06:51 -0300

    comment

--- C/tests/TestBinder.h
+++ C/tests/TestBinder.h
@@ -46,24 +46,15 @@ public:
             + 0100-0199 ->
             + 0200-0299 ->
 
-        Globals and locals:
-            + 1000-1049 -> objects (plain)
-            + 1050-1099 -> `const' objects
-            + 1100-1149 -> pointers
-            + 1150-1199 -> `const' object pointers
-            + 1200-1249 -> `const' object `const' pointers
-            + 1250-1299 -> `const' pointers
-            + 1300-
-
-        Fields:
-            + 2000-2049 -> objects (plain)
-            + 2050-2099 -> `const' objects
-            + 2100-2149 -> pointers
-            + 2150-2199 -> `const' object pointers
-            + 2200-2249 -> `const' object `const' pointers
-            + 2250-2299 -> `const' pointers
-            + 2300-
-
+        Globals and locals: 1xxx
+        Fields:             2xxx
+            + [1-2]000-[1-2]049 -> objects (plain)
+            + [1-2]050-[1-2]099 -> `const' objects
+            + [1-2]100-[1-2]149 -> pointers
+            + [1-2]150-[1-2]199 -> `const' object pointers
+            + [1-2]200-[1-2]249 -> `const' object `const' pointers
+            + [1-2]250-[1-2]299 -> `const' pointers
+            + [1-2]300-
      */
 
     void case0001();
