2520139d3e0066db2e2e105b355fc62e4126f725
Leandro T. C. Melo <LTCMELO@GMAIL.COM>
2022-04-25 19:43:01 -0300

    more new docs

---
+++ api-docs/html/search/enums_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['variant_977',['Variant',['../classpsy_1_1_c_1_1_integer_constant.html#a637fd18d386ab30d8f8fd51023b8cbdd',1,'psy::C::IntegerConstant::Variant()'],['../classpsy_1_1_c_1_1_floating_constant.html#a9f3b4625af413f368c32a7f816dd2106',1,'psy::C::FloatingConstant::Variant()'],['../classpsy_1_1_c_1_1_character_constant.html#a6c5b7e2adb38c1921d836641903c8612',1,'psy::C::CharacterConstant::Variant()'],['../classpsy_1_1_c_1_1_string_literal.html#a9d1215d6b2a863b16523c7582d546579',1,'psy::C::StringLiteral::Variant()']]]
+];
